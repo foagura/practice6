f56b65f7df37c994e1abf7b5b2d99cafd373872e
foagura <foagura_0416@yahoo.co.jp>
2024-09-07 20:30:43 +0900

    first commit

---
+++ src/Practice6.java
@@ -0,0 +1,12 @@
+public class Practice6 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        
+        /* 関係演算子の練習 */
+        System.out.println("a == b:" + (a == b));
+        System.out.println("a != b:" + (a != b));
+        System.out.println("a > b:" + (a > b));
+        System.out.println("a < b:" + (a < b));
+    }
+}
